433beee75145623de73f81168a7ce7c4d0e492c4
TGyAkos <t.gyula.akos@gmail.com>
2023-11-15 12:10:24 +0100

    done

---
+++ haromszog/Haromszog.cs
@@ -0,0 +1,51 @@
+﻿namespace Haromszog;
+
+internal class Haromszog
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("asdads");
+        var test = new Test();
+    }
+}
+
+internal class HaromszogData
+{
+    public int[] A { get; set; }
+    public int[] B { get; set; }
+    public int[] C { get; set; }
+
+    public double ABH { get; set; }
+    public double BCH { get; set; }
+    public double ACH { get; set; }
+
+    public void CalcHossz()
+    {
+        ABH = Math.Sqrt(Math.Pow(B[0] - A[0], 2.0) + Math.Pow(B[1] - A[1], 2.0));
+        BCH = Math.Sqrt(Math.Pow(C[0] - B[0], 2.0) + Math.Pow(C[1] - B[1], 2.0));
+        ACH = Math.Sqrt(Math.Pow(C[0] - A[0], 2.0) + Math.Pow(C[1] - A[1], 2.0));
+    }
+
+    public bool IsEgyenloSzaru()
+    {
+        if (ABH == BCH || ABH == ACH || BCH == ACH) return true;
+        return false;
+    }
+
+    public bool IsSzabalyos()
+    {
+        if (ABH == BCH && BCH == ACH) return true;
+        return false;
+    }
+
+    public double CalcKerulet()
+    {
+        return ABH + BCH + ACH;
+    }
+
+    public double CalcTerulet()
+    {
+        var s = (ABH + BCH + ACH) / 2;
+        return Math.Sqrt(s * (s - ABH) * (s - BCH) * (s - ACH));
+    }
+}
---
+++ haromszog/Test.cs
@@ -0,0 +1,21 @@
+﻿namespace Haromszog;
+
+public class Test
+{
+    private readonly HaromszogData _HaromszogData = new();
+
+    public Test()
+    {
+        _HaromszogData.A = new[] { 0, 0 };
+        _HaromszogData.B = new[] { 10, 0 };
+        _HaromszogData.C = new[] { 5, 10 };
+
+        _HaromszogData.CalcHossz();
+
+        Console.WriteLine(_HaromszogData.IsSzabalyos());
+        Console.WriteLine(_HaromszogData.IsEgyenloSzaru());
+
+        Console.WriteLine(_HaromszogData.CalcKerulet());
+        Console.WriteLine(_HaromszogData.CalcTerulet());
+    }
+}
